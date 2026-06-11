d63577725c21110c2acabb7888c9b5e343327448
Sophia Hellen <sophia.silveira@alunos.ufersa.edu.br>
2026-06-11 10:53:59 -0300

    pwa mobile

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function mostrarDeclaracao() {
   document.getElementById("declaracao").innerHTML =
     "Eu te amo muito! Obrigado por existir na minha vida. ❤️";
 }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+    .then(() => {
+      console.log("Service Worker registrado com sucesso!");
+    })
+    .catch(error => {
+      console.log("Erro ao registrar Service Worker:", error);
+    });
+}
